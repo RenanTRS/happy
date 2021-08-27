11a3db1e1a77cf4ad72ae77a9b1cd4e40b32a6bb
Renan Souza <renantrsouza@gmail.com>
2021-08-27 13:41:30 -0400

    add mapa

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,23 @@
+//Inserindo coordenadas             lat     long    zoom
+const map = L.map('mapid').setView([-27.2210624,-49.6455527], 15);
+
+//openstreetmap - mapa gratuito
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+//creat popup
+const popup = L.popup({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
+
+//popup
+L.marker([-27.2210624,-49.6455527], {icon: icon}).addTo(map).bindPopup(popup);
