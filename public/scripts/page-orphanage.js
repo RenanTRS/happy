900004b0a2f29eaa48b814fa27d8926c7561de4a
Renan Souza <renantrsouza@gmail.com>
2021-09-01 11:31:05 -0400

    Modifica a imagem clicando nos botões

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -21,3 +21,25 @@ const icon = L.icon({
 
 //create and add marker
 L.marker([-27.2210624,-49.6455527], {icon: icon}).addTo(map);
+
+
+//image gallery
+function selectImage(event){
+    const button = event.currentTarget;
+    
+    //Remover todas as classes .active
+    const buttons = document.querySelectorAll('.images button');
+    buttons.forEach((button)=>{
+        button.classList.remove('active');
+    });
+
+    //Selecionar image clicada
+    const image = button.children[0]; //pegando o primeiro filho de button
+    const imageContainer = document.querySelector('.orphanage-details > img'); //Pegando a imagem grande do container
+
+    //Atualizar o container de image
+    imageContainer.src = image.src; //Mudando a imagem grande do container
+
+    //Adicionar a classe .active para este botão
+    button.classList.add('active');
+}
