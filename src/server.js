3f2bd5184919af7eeb66925daea662fee5357387
Renan Souza <renantrsouza@gmail.com>
2021-09-11 11:30:06 -0400

    Cria rota para o index

---
+++ src/server.js
@@ -0,0 +1,18 @@
+//Importar dependência
+const express = require('express');
+const path = require('path'); //Serve para identificar o sistema operacional e muda a posição da barra
+
+//Inicia o express
+const server = express();
+
+//Utilizando os arquivos estáticos
+server.use(express.static('public'));
+
+//Cria uma rota
+server.get('/', (req, res)=>{
+
+    return res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+//Liga o servidor
+server.listen(5500);
