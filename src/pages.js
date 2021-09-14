ee99ec02c49b9ac95886ac49df7d1c860981c1db
Renan Souza <renantrsouza@gmail.com>
2021-09-14 11:26:35 -0400

    importa dados de fakedata.js

--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,11 @@
+const orphanages = require('./database/fakedata.js');
+
 module.exports = {
     index(req, res){
         return res.render('index');
     },
     orphanages(req, res){
-        return res.render('orphanages');
+        return res.render('orphanages', {orphanages});
     },
     orphanage(req, res){
         return res.render('orphanage');
