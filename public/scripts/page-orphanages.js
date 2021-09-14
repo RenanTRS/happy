02bf9228c586255e7265a7fc4d06c8d0d81d764c
Renan Souza <renantrsouza@gmail.com>
2021-09-14 10:26:43 -0400

    Apaga ./public dos diretórios restantes

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -17,7 +17,7 @@ const popup = L.popup({
     className: "map-popup",
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="/images/arrow-white.svg"></a>')
 
 //popup
 L.marker([-27.2210624,-49.6455527], {icon: icon}).addTo(map).bindPopup(popup);
