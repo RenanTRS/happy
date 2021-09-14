9a1af991a31af92469a29e2388963515eb8981de
Renan Souza <renantrsouza@gmail.com>
2021-09-14 11:22:56 -0400

    Adiciona dados

---
+++ src/database/fakedata.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+        id: "1",
+        lat: "-27.2210624",
+        lng: "-49.6455527",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: ["https://images.unsplash.com/photo-1597095556745-7bcd893840ac?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8aWR8fHx8fHwxNjMxNjMyNDMy&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
+
+        "https://images.unsplash.com/photo-1626103999420-4187ccec0cf7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8aWR8fHx8fHwxNjMxNjMyNTEw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h às 18h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id: "2",
+        lat: "-27.2210624",
+        lng: "-49.6555527",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: ["https://images.unsplash.com/photo-1597095556745-7bcd893840ac?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8aWR8fHx8fHwxNjMxNjMyNDMy&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
+
+        "https://images.unsplash.com/photo-1626103999420-4187ccec0cf7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8aWR8fHx8fHwxNjMxNjMyNTEw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h às 18h",
+        open_on_weekends: "0"
+    }
+];
