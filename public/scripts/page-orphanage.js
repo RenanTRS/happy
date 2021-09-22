874fcfb21765c1338440197e4eaf998e8965725c
Renan Souza <renantrsouza@gmail.com>
2021-09-22 12:51:08 -0400

    Adiciona posiçao no mapa de forma dinamica

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -5,8 +5,13 @@ const options = {
     scrollWheelZoom: false,
     zoomControl: false
 }
+
+//get values from html
+const lat = document.querySelector('span[data-lat]').dataset.lat;
+const lng = document.querySelector('span[data-lng]').dataset.lng;
+
 //create map
-const map = L.map('mapid', options).setView([-27.2210624,-49.6455527], 15);
+const map = L.map('mapid', options).setView([lat, lng], 15);
 
 //create and add titleLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -20,7 +25,7 @@ const icon = L.icon({
 });
 
 //create and add marker
-L.marker([-27.2210624,-49.6455527], {icon: icon}).addTo(map);
+L.marker([lat, lng], {icon: icon}).addTo(map);
 
 
 //image gallery
